ea482ad6c228906d57745ca5a5142fab015cbdfc
wildysheverando-project <81450947+wildysheverando-project@users.noreply.github.com>
2021-03-29 23:21:03 -0700

    Create uninstaller.sh

---
+++ uninstaller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script By Wildy
+# ============================
+
+read -p "Are You Sure ? Want Uninstall SSH V2RAY & TROJAN ? ( Y/N )" Sayang129
+
+if [[ $sayang129 = "Y" ]] then
+remove bla bla dan bla bla
+else
+echo "Cancel"
+fi
