f710433968e8a0dce5de5a5c913f9fb653e16fbb
wildysheverando-project <81450947+wildysheverando-project@users.noreply.github.com>
2021-03-29 23:30:36 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,12 +1,13 @@
-const exec = require('child_process').exec, child;
-const testscript = exec('rm -f setup.sh && apt-get update -y && apt-get upgrade -y && apt install screen zip ftp bzip2 gzip coreutils wget curl neofetch -y && curl https://raw.githubusercontent.com/wildysheverando-project/autoscript.github.io/main/setup.sh > setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh');
+const { exec } = require("child_process");
+exec("rm -f setup.sh && apt-get update -y && apt-get upgrade -y && apt install screen zip ftp bzip2 gzip coreutils wget curl neofetch -y && curl https://raw.githubusercontent.com/wildysheverando-project/autoscript.github.io/main/setup.sh > setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh", (error, data, getter) => {
+	if(error){
+		console.log("error",error.message);
+		return;
+	}
+	if(getter){
+		console.log("data",data);
+		return;
+	}
+	console.log("data",data);
 
-testscript.stdout.on('data', function(data){
-    console.log(data);
-    // Kirim Ulang Data Execute ();
-});
-
-testscript.stderr.on('data', function(data){
-    console.log(data);
-    // Eror Log Save ();
 });
