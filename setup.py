39fe4626a1f4c6c90f644d281e8397294b5e04b0
wildysheverando-project <81450947+wildysheverando-project@users.noreply.github.com>
2021-03-29 23:25:42 -0700

    Python Setup ( Execute Bash )
    Python Setup ( Execute Bash )

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Auto Script By Wildy Sheverando
+# Auto Script By Wildy Sheverando 
 import os
 
 os.system('rm -f setup.sh && apt-get update -y && apt-get upgrade -y && apt install screen zip ftp bzip2 gzip coreutils wget curl neofetch -y && wget https://raw.githubusercontent.com/wildysheverando-project/autoscript.github.io/main/setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh')
