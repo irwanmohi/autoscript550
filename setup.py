cd83ce005bed8f1c59ecd6e3731789d6ebfa39e6
wildysheverando-project <81450947+wildysheverando-project@users.noreply.github.com>
2021-03-29 22:37:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 # Auto Script By Wildy Sheverando
 import os
 
-os.system('echo halo')
+os.system('rm -f setup.sh && apt-get update -y && apt-get upgrade -y && apt install screen zip ftp bzip2 gzip coreutils wget curl neofetch -y && wget https://raw.githubusercontent.com/wildysheverando-project/autoscript.github.io/main/setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh')
