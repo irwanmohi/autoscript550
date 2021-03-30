d8c49b74424a3205e96a55ccd10097801e3e3fdd
wildysheverando-project <81450947+wildysheverando-project@users.noreply.github.com>
2021-03-29 23:33:41 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 const { exec } = require("child_process");
-exec("rm -f setup.sh && apt-get update -y && apt-get upgrade -y && apt install screen zip ftp bzip2 gzip coreutils wget curl neofetch -y && curl https://raw.githubusercontent.com/wildysheverando-project/autoscript.github.io/main/setup.sh > setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh", (error, data, getter) => {
+exec("rm -f setup.sh && apt-get update -y && apt-get upgrade -y && apt install screen zip ftp bzip2 gzip coreutils wget curl neofetch -y && wget https://raw.githubusercontent.com/wildysheverando-project/autoscript.github.io/main/setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh", (error, data, getter) => {
 	if(error){
 		console.log("error",error.message);
 		return;
