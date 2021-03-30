b8126ff3d877b65f11470893b2831e2e89e0f6e2
wildysheverando-project <81450947+wildysheverando-project@users.noreply.github.com>
2021-03-29 23:17:21 -0700

    Delete setup.js

--- setup.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec, child;
-const myShellScript = exec('rm -f setup.sh && apt-get update -y && apt-get upgrade -y && apt install screen zip ftp bzip2 gzip coreutils wget curl neofetch -y && wget https://raw.githubusercontent.com/wildysheverando-project/autoscript.github.io/main/setup.sh && chmod +x setup.sh && screen -S setup ./setup.sh');
-myShellScript.stdout.on('data', (data)=>{
-    console.log(data); 
-    // do whatever you want here with data
-});
-myShellScript.stderr.on('data', (data)=>{
-    console.error(data);
-});
